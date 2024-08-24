72cf152a9bbdbbb65a335bf624b9f6437a5bf9cd
Binaya321 <Binayapaudel782@gmail.com>
2024-08-24 19:48:59 -0400

    Draft 1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,14 +1,31 @@
+import satData from "./satData";
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
      <tr>
-       <td>Row Data TBD</td>
+       <td>
+       {
+          sat.map((data => {
+            return data.id ? (
+              <td key = {data.id}>
+                {data.name}
+                {data.type}
+                {data.launchDate}
+                {data.operational = true ? ("Active"): ("Inactive")}
+                </td>  
+            ) : null;
+}))
+      }
+       </td>
      </tr>
      </tbody>
    </table>
